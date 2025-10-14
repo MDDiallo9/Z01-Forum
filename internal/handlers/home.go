cacc73d98723f9032d81d00ea4a5e28080c1c1f9
Mohamed LN-WSL <114567817+MDDiallo9@users.noreply.github.com>
2025-10-14 18:34:56 +0200

    feat(register): added register method to user model with UUID and bcrypt

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"forum/internal/app"
 	"net/http"
+	"log"
 )
 
 func Home(f *app.Application) http.HandlerFunc {
@@ -10,6 +11,11 @@ func Home(f *app.Application) http.HandlerFunc {
         if f.InfoLog != nil {
             f.InfoLog.Println("home page")
         }
+		test,err := f.Users.Register("Cudder","bla@bla.col","fsdfdsf","avatar.jpg",1)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println(test)
         w.Write([]byte("Welcome to the Forum!"))
     }
 }
--- internal/models/users.go
+++ internal/models/users.go
@@ -4,15 +4,45 @@ import (
 	"database/sql"
 	/* "errors"
 	"time" */
+	"golang.org/x/crypto/bcrypt"
+	"github.com/google/uuid"
 )
 
 type UsersModel struct {
 	DB *sql.DB
 }
 
-/* func (m *UsersModel) Create()(int,error){
-	UUID := "sdfsdfs"
+func HashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
+}
 
-	statement := `INSERT INTO users (id,username,email,password,avatar,role)`
-} */
+func CheckPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
+
+func (m *UsersModel) Register(username,email,password,avatar string,role int)(string,error){
+	UUID := uuid.New().String()
+	hashedPw,err := HashPassword(password)
+	if err != nil{
+		return "",err
+	}
+	session_id := "0abcd123"
+	statement := `INSERT INTO users (id,username,email,password,avatar,role,session_id,session_created_at) 
+	VALUES(?,?,?,?,?,?,?,datetime())`
+
+	result, err := m.DB.Exec(statement, UUID,username,email, hashedPw,avatar, role,session_id)
+	if err != nil {
+		return "", err
+	}
+	// Getting the last insert ID to be sure the INSERT worked
+	id, err := result.LastInsertId()
+	if err != nil {
+		return string(id), err
+	}
+
+	// Returned ID is int64 type , we convert it before returning
+	return UUID, nil
+}
 
