4a6a4272ef99d602a6bbd1ed4306bbd584e3f9fb
Mohamed LN-WSL <114567817+MDDiallo9@users.noreply.github.com>
2025-10-16 13:04:14 +0200

    Added a temp  create post template, and new methods to the posts handler

--- internal/app/forum.go
+++ internal/app/forum.go
@@ -10,6 +10,7 @@ type Application struct {
 	InfoLog *log.Logger
 	Users *models.UsersModel
 	Posts *models.PostsModel
+	Attachments *models.AttachmentsModel
 	TemplateData  TemplateData
 }
 
@@ -17,7 +18,7 @@ type TemplateData struct {
 	Form any
 }
 
-func NewApplication(info, errLog *log.Logger, users *models.UsersModel,posts *models.PostsModel) *Application {
-    return &Application{InfoLog: info, ErrorLog: errLog, Users: users,Posts:posts}
+func NewApplication(info, errLog *log.Logger, users *models.UsersModel,posts *models.PostsModel,attachments *models.AttachmentsModel) *Application {
+    return &Application{InfoLog: info, ErrorLog: errLog, Users: users,Posts:posts,Attachments: attachments}
 }
 
